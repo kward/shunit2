a5a54cfcdc773016a094db78869de986ce6c3805
kate.ward <kate.ward@forestent.com>
2008-09-03 22:20:42 +0000

    got the shell failure catch working again

--- source/2.1/src/shell/shunit2_test_output.sh
+++ source/2.1/src/shell/shunit2_test_output.sh
@@ -41,6 +41,11 @@ EOF
   assertTrue 'test count message was not generated' $?
   grep '^FAILED' "${stdoutF}" >/dev/null
   assertTrue 'failure message was not generated' $?
+  #echo ---
+  #cat ${stdoutF}
+  #echo ---
+  #cat ${stderrF}
+  #echo ---
 }
 
 #------------------------------------------------------------------------------
