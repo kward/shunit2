85dfdd62a902bdccc5bdccf233e2ad311b94ed10
kate.ward@forestent.com <kate.ward@forestent.com@0b216afc-fc4a-0410-87c2-ade1b7cef105>
2008-10-27 18:27:46 +0000

    issue# 8: demonstrate extra quoting requirements for macros

--- trunk/source/2.1/src/shell/shunit2_test_macros.sh
+++ trunk/source/2.1/src/shell/shunit2_test_macros.sh
@@ -25,95 +25,212 @@ testAssertEquals()
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
   assertTrue '_ASSERT_EQUALS_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
 
-  ( ${_ASSERT_EQUALS_} 'some msg' 'x' 'y' >"${stdoutF}" 2>"${stderrF}" )
+  ( ${_ASSERT_EQUALS_} '"some msg"' 'x' 'y' >"${stdoutF}" 2>"${stderrF}" )
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
   assertTrue '_ASSERT_EQUALS_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
 }
 
-testLineNo()
+testAssertNotEquals()
 {
   # start skipping if LINENO not available
   [ -z "${LINENO:-}" ] && startSkipping
-
-  ### asserts
 
   ( ${_ASSERT_NOT_EQUALS_} 'x' 'x' >"${stdoutF}" 2>"${stderrF}" )
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
   assertTrue '_ASSERT_NOT_EQUALS_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
 
-  ( ${_ASSERT_NULL_} 'x' >"${stdoutF}" 2>"${stderrF}" )
+  ( ${_ASSERT_NOT_EQUALS_} '"some msg"' 'x' 'x' >"${stdoutF}" 2>"${stderrF}" )
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
-  assertTrue '_ASSERT_NULL_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  assertTrue '_ASSERT_NOT_EQUALS_ failure' ${rtrn}
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+}
 
-  ( ${_ASSERT_NOT_NULL_} '' >"${stdoutF}" 2>"${stderrF}" )
-  grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
-  rtrn=$?
-  assertTrue '_ASSERT_NOT_NULL_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+testSame()
+{
+  # start skipping if LINENO not available
+  [ -z "${LINENO:-}" ] && startSkipping
 
   ( ${_ASSERT_SAME_} 'x' 'y' >"${stdoutF}" 2>"${stderrF}" )
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
   assertTrue '_ASSERT_SAME_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+
+  ( ${_ASSERT_SAME_} '"some msg"' 'x' 'y' >"${stdoutF}" 2>"${stderrF}" )
+  grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
+  rtrn=$?
+  assertTrue '_ASSERT_SAME_ failure' ${rtrn}
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+}
+
+testNotSame()
+{
+  # start skipping if LINENO not available
+  [ -z "${LINENO:-}" ] && startSkipping
 
   ( ${_ASSERT_NOT_SAME_} 'x' 'x' >"${stdoutF}" 2>"${stderrF}" )
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
   assertTrue '_ASSERT_NOT_SAME_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+
+  ( ${_ASSERT_NOT_SAME_} '"some msg"' 'x' 'x' >"${stdoutF}" 2>"${stderrF}" )
+  grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
+  rtrn=$?
+  assertTrue '_ASSERT_NOT_SAME_ failure' ${rtrn}
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+}
+
+testNull()
+{
+  # start skipping if LINENO not available
+  [ -z "${LINENO:-}" ] && startSkipping
+
+  ( ${_ASSERT_NULL_} 'x' >"${stdoutF}" 2>"${stderrF}" )
+  grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
+  rtrn=$?
+  assertTrue '_ASSERT_NULL_ failure' ${rtrn}
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+
+  ( ${_ASSERT_NULL_} '"some msg"' 'x' >"${stdoutF}" 2>"${stderrF}" )
+  grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
+  rtrn=$?
+  assertTrue '_ASSERT_NULL_ failure' ${rtrn}
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+}
+
+testNotNull()
+{
+  # start skipping if LINENO not available
+  [ -z "${LINENO:-}" ] && startSkipping
+
+  ( ${_ASSERT_NOT_NULL_} '' >"${stdoutF}" 2>"${stderrF}" )
+  grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
+  rtrn=$?
+  assertTrue '_ASSERT_NOT_NULL_ failure' ${rtrn}
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+
+  ( ${_ASSERT_NOT_NULL_} '"some msg"' '""' >"${stdoutF}" 2>"${stderrF}" )
+  grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
+  rtrn=$?
+  assertTrue '_ASSERT_NOT_NULL_ w/ msg failure' ${rtrn}
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stdoutF}" "${stderrF}" >&2
+}
+
+testAssertTrue()
+{
+  # start skipping if LINENO not available
+  [ -z "${LINENO:-}" ] && startSkipping
 
   ( ${_ASSERT_TRUE_} ${SHUNIT_FALSE} >"${stdoutF}" 2>"${stderrF}" )
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
   assertTrue '_ASSERT_TRUE_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+
+
+  ( ${_ASSERT_TRUE_} '"some msg"' ${SHUNIT_FALSE} >"${stdoutF}" 2>"${stderrF}" )
+  grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
+  rtrn=$?
+  assertTrue '_ASSERT_TRUE_ failure' ${rtrn}
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+}
+
+testAssertFalse()
+{
+  # start skipping if LINENO not available
+  [ -z "${LINENO:-}" ] && startSkipping
 
   ( ${_ASSERT_FALSE_} ${SHUNIT_TRUE} >"${stdoutF}" 2>"${stderrF}" )
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
   assertTrue '_ASSERT_FALSE_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
 
-  ( ${_ASSERT_FALSE_} ${SHUNIT_TRUE} >"${stdoutF}" 2>"${stderrF}" )
+  ( ${_ASSERT_FALSE_} '"some msg"' ${SHUNIT_TRUE} >"${stdoutF}" 2>"${stderrF}" )
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
   assertTrue '_ASSERT_FALSE_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+}
 
-  ### failures
+testFail()
+{
+  # start skipping if LINENO not available
+  [ -z "${LINENO:-}" ] && startSkipping
 
   ( ${_FAIL_} >"${stdoutF}" 2>"${stderrF}" )
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
   assertTrue '_FAIL_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+
+  ( ${_FAIL_} '"some msg"' >"${stdoutF}" 2>"${stderrF}" )
+  grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
+  rtrn=$?
+  assertTrue '_FAIL_ failure' ${rtrn}
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+}
+
+testFailNotEquals()
+{
+  # start skipping if LINENO not available
+  [ -z "${LINENO:-}" ] && startSkipping
 
   ( ${_FAIL_NOT_EQUALS_} 'x' 'y' >"${stdoutF}" 2>"${stderrF}" )
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
   assertTrue '_FAIL_NOT_EQUALS_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+
+  ( ${_FAIL_NOT_EQUALS_} '"some msg"' 'x' 'y' >"${stdoutF}" 2>"${stderrF}" )
+  grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
+  rtrn=$?
+  assertTrue '_FAIL_NOT_EQUALS_ failure' ${rtrn}
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+}
+
+testFailSame()
+{
+  # start skipping if LINENO not available
+  [ -z "${LINENO:-}" ] && startSkipping
 
   ( ${_FAIL_SAME_} 'x' 'x' >"${stdoutF}" 2>"${stderrF}" )
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
   assertTrue '_FAIL_SAME_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+
+  ( ${_FAIL_SAME_} '"some msg"' 'x' 'x' >"${stdoutF}" 2>"${stderrF}" )
+  grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
+  rtrn=$?
+  assertTrue '_FAIL_SAME_ failure' ${rtrn}
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+}
+
+testFailNotSame()
+{
+  # start skipping if LINENO not available
+  [ -z "${LINENO:-}" ] && startSkipping
 
   ( ${_FAIL_NOT_SAME_} 'x' 'y' >"${stdoutF}" 2>"${stderrF}" )
   grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
   rtrn=$?
   assertTrue '_FAIL_NOT_SAME_ failure' ${rtrn}
-  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}">&2
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
+
+  ( ${_FAIL_NOT_SAME_} '"some msg"' 'x' 'y' >"${stdoutF}" 2>"${stderrF}" )
+  grep '^ASSERT:\[[0-9]*\] *' "${stdoutF}" >/dev/null
+  rtrn=$?
+  assertTrue '_FAIL_NOT_SAME_ failure' ${rtrn}
+  [ ${rtrn} -ne ${SHUNIT_TRUE} ] && cat "${stderrF}" >&2
 }
 
 #------------------------------------------------------------------------------
