ae6780ce28a405c58d482e3f06b013efb7f95d27
kate.ward@forestent.com <kate.ward@forestent.com@0b216afc-fc4a-0410-87c2-ade1b7cef105>
2011-04-27 22:35:31 +0000

    comment and output fixes in testLineNo()

--- trunk/source/2.1/examples/lineno_test.sh
+++ trunk/source/2.1/examples/lineno_test.sh
@@ -3,8 +3,9 @@
 
 testLineNo()
 {
-  # this assert will have line numbers included (e.g. "ASSERT:[123] ...")
-  echo "ae: ${_ASSERT_EQUALS_}"
+  # this assert will have line numbers included (e.g. "ASSERT:[123] ...") if
+  # they are supported.
+  echo "_ASSERT_EQUALS_ macro value: ${_ASSERT_EQUALS_}"
   ${_ASSERT_EQUALS_} 'not equal' 1 2
 
   # this assert will not have line numbers included (e.g. "ASSERT: ...")
