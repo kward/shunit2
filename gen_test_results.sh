83d9d2409ba0ca1e762922ee4974e3acaf7e4fe6
Kate Ward <kate.ward@forestent.com>
2017-09-28 23:27:58 +0200

    Fixed shellcheck warnings.

--- gen_test_results.sh
+++ gen_test_results.sh
@@ -10,8 +10,13 @@
 # Author: kate.ward@forestent.com (Kate Ward)
 # https://github.com/kward/shunit2
 #
-# Disable annoying ShellCheck checks.
-# shellcheck disable=SC1091,SC2006,SC2154
+### Disable annoying ShellCheck checks.
+## Source following.
+# shellcheck disable=SC1090,SC1091
+## FLAGS variables are dynamically created.
+# shellcheck disable=SC2154
+## Disagree with [ p ] && [ q ] vs [ p -a -q ] recommendation.
+# shellcheck disable=SC2166
 
 # Treat unset variables as an error.
 set -u
@@ -21,29 +26,25 @@ die() {
   exit 1
 }
 
-BASE_DIR=`dirname "$0"`
+BASE_DIR=$(dirname "$0")
 LIB_DIR="${BASE_DIR}/lib"
 
 ### Load libraries.
-# shellcheck source=lib/shflags
 . "${LIB_DIR}/shflags" || die 'unable to load shflags library'
-# shellcheck source=lib/shlib
 . "${LIB_DIR}/shlib" || die 'unable to load shlib library'
-# shellcheck source=lib/versions
 . "${LIB_DIR}/versions" || die 'unable to load versions library'
 
 # Redefining BASE_DIR now that we have the shlib functions. We need BASE_DIR so
 # that we can properly load things, even in the event that this script is called
 # from a different directory.
-BASE_DIR=`shlib_relToAbsPath "${BASE_DIR}"`
+BASE_DIR=$(shlib_relToAbsPath "${BASE_DIR}")
 
 ### Load external flags.
-# shellcheck source=./gen_test_results.flags
 . "${BASE_DIR}/gen_test_results.flags"
 
 # Define flags.
-os_name=`versions_osName |sed 's/ /_/g'`
-os_version=`versions_osVersion`
+os_name=$(versions_osName |sed 's/ /_/g')
+os_version=$(versions_osVersion)
 
 DEFINE_boolean force false 'force overwrite' f
 DEFINE_string output_dir "${TMPDIR}" 'output dir' d
@@ -52,40 +53,41 @@ DEFINE_boolean dry_run false "suppress logging to a file" n
 
 main() {
   # Determine output filename.
+  # shellcheck disable=SC2154
   output="${FLAGS_output_dir:+${FLAGS_output_dir}/}${FLAGS_output_file}"
-  output=`shlib_relToAbsPath "${output}"`
+  output=$(shlib_relToAbsPath "${output}")
 
   # Checks.
   [ -n "${FLAGS_suite:-}" ] || die 'suite flag missing'
 
-  if [ ${FLAGS_dry_run} -eq ${FLAGS_FALSE} -a -f "${output}" ]; then
-    if [ ${FLAGS_force} -eq ${FLAGS_TRUE} ]; then
+  if [ "${FLAGS_dry_run}" -eq "${FLAGS_FALSE}" -a -f "${output}" ]; then
+    if [ "${FLAGS_force}" -eq "${FLAGS_TRUE}" ]; then
       rm -f "${output}"
     else
       echo "not overwriting '${output}'" >&2
-      exit ${FLAGS_ERROR}
+      exit "${FLAGS_ERROR}"
     fi
   fi
-  if [ ${FLAGS_dry_run} -eq ${FLAGS_FALSE} ]; then
+  if [ "${FLAGS_dry_run}" -eq "${FLAGS_FALSE}" ]; then
     touch "${output}" 2>/dev/null || die "unable to write to '${output}'"
   fi
 
   # Run tests.
   (
-    if [ ${FLAGS_dry_run} -eq ${FLAGS_FALSE} ]; then
-      ./${FLAGS_suite} |tee "${output}"
+    if [ "${FLAGS_dry_run}" -eq "${FLAGS_FALSE}" ]; then
+      "./${FLAGS_suite}" |tee "${output}"
     else
-      ./${FLAGS_suite}
+      "./${FLAGS_suite}"
     fi
   )
 
-  if [ ${FLAGS_dry_run} -eq ${FLAGS_FALSE} ]; then
+  if [ "${FLAGS_dry_run}" -eq "${FLAGS_FALSE}" ]; then
     echo >&2
     echo "Output written to '${output}'." >&2
   fi
 }
 
 FLAGS "$@" || exit $?
-[ ${FLAGS_help} -eq ${FALSE} ] || exit
+[ "${FLAGS_help}" -eq "${FLAGS_FALSE}" ] || exit
 eval set -- "${FLAGS_ARGV}"
 main "${@:-}"
