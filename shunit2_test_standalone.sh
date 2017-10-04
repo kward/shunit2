0d6605d592e90b40db8b58583a9ee8ed25b3524c
Kate Ward <kate.ward@forestent.com>
2017-10-04 22:53:43 +0200

    Setup shellcheck for shunit2_test_standalone.sh.

--- shunit2_test_standalone.sh
+++ shunit2_test_standalone.sh
@@ -1,41 +1,38 @@
 #! /bin/sh
-# $Id$
 # vim:et:ft=sh:sts=2:sw=2
 #
-# Copyright 2010 Kate Ward. All Rights Reserved.
-# Released under the LGPL (GNU Lesser General Public License)
-# Author: kate.ward@forestent.com (Kate Ward)
-#
 # shUnit2 unit test for standalone operation.
+#
+# Copyright 2010-2017 Kate Ward. All Rights Reserved.
+# Released under the Apache 2.0 license.
+#
+# Author: kate.ward@forestent.com (Kate Ward)
+# https://github.com/kward/shunit2
 #
 # This unit test is purely to test that calling shunit2 directly, while passing
 # the name of a unit test script, works. When run, this script determines if it
 # is running as a standalone program, and calls main() if it is.
+#
+### ShellCheck http://www.shellcheck.net/
+# $() are not fully portable (POSIX != portable).
+#   shellcheck disable=SC2006
+# Disable source following.
+#   shellcheck disable=SC1090,SC1091
 
-ARGV0=`basename "$0"`
+ARGV0="`basename "$0"`"
 
-# load test helpers
+# Load test helpers.
 . ./shunit2_test_helpers
 
-#------------------------------------------------------------------------------
-# suite tests
-#
-
-testStandalone()
-{
-  assertTrue ${SHUNIT_TRUE}
+testStandalone() {
+  assertTrue "${SHUNIT_TRUE}"
 }
 
-#------------------------------------------------------------------------------
-# main
-#
-
-main()
-{
+main() {
   ${TH_SHUNIT} "${ARGV0}"
 }
 
-# are we running as a standalone?
+# Are we running as a standalone?
 if [ "${ARGV0}" = 'shunit2_test_standalone.sh' ]; then
   if [ $# -gt 0 ]; then main "$@"; else main; fi
 fi
