296ecacbed2c98d0f3541033349da764258e0f11
Kate Ward <kate.ward@forestent.com>
2018-11-06 20:53:21 +0100

    Reworked color detection, adding unit tests.

--- shunit2_misc_test.sh
+++ shunit2_misc_test.sh
@@ -248,8 +248,8 @@ EOF
   assertEquals 'testABC test_def testG3 test4 test5 test6 test7 test8 test-9' "${actual}"
 }
 
-# Test that certain external commands sometimes "stubbed" by users
-# are escaped. See Issue #54.
+# Test that certain external commands sometimes "stubbed" by users are escaped.
+# See Issue #54.
 testProtectedCommands() {
   for c in mkdir rm cat chmod; do
     grep "^[^#]*${c} " "${TH_SHUNIT}" | grep -qv "command ${c}"
@@ -261,6 +261,37 @@ testProtectedCommands() {
   assertFalse "call to . not protected somewhere" $?
 }
 
+mock_tput() {
+  if [ -z "${TERM}" ]; then
+    echo 'tput: No value for $TERM and no -T specified'
+    return 2
+  fi
+  if [ $1 = 'colors' ]; then
+    echo 256
+    return 0
+  fi
+  return 1
+}
+
+testColors() {
+  while read -r desc cmd colors; do
+    SHUNIT_CMD_TPUT=${cmd}
+    got=`_shunit_colors`
+    want=${colors}
+    assertEquals ${got} ${want}
+  done <<'EOF'
+missing missing_tput 16
+mock mock_tput 256
+EOF
+}
+
+testColorsWitoutTERM() {
+  SHUNIT_CMD_TPUT='mock_tput'
+  got=`TERM= _shunit_colors`
+  want=16
+  assertEquals ${got} ${want}
+}
+
 setUp() {
   for f in "${stdoutF}" "${stderrF}"; do
     cp /dev/null "${f}"
@@ -268,6 +299,8 @@ setUp() {
 
   # Reconfigure coloring as some tests override default behavior.
   _shunit_configureColor "${SHUNIT_COLOR_DEFAULT}"
+
+  SHUNIT_CMD_TPUT=${__SHUNIT_CMD_TPUT}
 }
 
 oneTimeSetUp() {
