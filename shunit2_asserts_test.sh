5542ca81d8b27ac5fed77265ea6d67906ffd2c8e
David DIDIER <c_inconnu2@yahoo.fr>
2019-02-14 22:22:51 +0100

    Add file and directory assertions

--- shunit2_asserts_test.sh
+++ shunit2_asserts_test.sh
@@ -375,6 +375,110 @@ testTooManyArguments() {
   done
 }
 
+testAssertFileExists() {
+  ( assertFileExists $TH_EXISTING_FILE >"${stdoutF}" 2>"${stderrF}" )
+  th_assertTrueWithNoOutput 'file exists' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileExists "${MSG}" $TH_EXISTING_FILE >"${stdoutF}" 2>"${stderrF}" )
+  th_assertTrueWithNoOutput 'file exists, with msg' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileExists $TH_NON_EXISTING_FILE >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithOutput 'file does not exist' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileExists "${MSG}" $TH_NON_EXISTING_FILE >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithOutput 'file does not exist, with msg' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileExists $TH_EXISTING_DIRECTORY >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithOutput 'directory exists' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileExists "${MSG}" $TH_EXISTING_DIRECTORY >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithOutput 'directory exists, with msg' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileExists >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithError 'too few arguments' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileExists arg1 arg2 arg3 >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithError 'too many arguments' $? "${stdoutF}" "${stderrF}"
+}
+
+testAssertFileDoesNotExist() {
+  ( assertFileDoesNotExist $TH_NON_EXISTING_FILE >"${stdoutF}" 2>"${stderrF}" )
+  th_assertTrueWithNoOutput 'file does not exist' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileDoesNotExist "${MSG}" $TH_NON_EXISTING_FILE >"${stdoutF}" 2>"${stderrF}" )
+  th_assertTrueWithNoOutput 'file does not exist, with msg' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileDoesNotExist $TH_EXISTING_FILE >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithOutput 'file exists' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileDoesNotExist "${MSG}" $TH_EXISTING_FILE >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithOutput 'file exists, with msg' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileDoesNotExist $TH_EXISTING_DIRECTORY >"${stdoutF}" 2>"${stderrF}" )
+  th_assertTrueWithNoOutput 'directory exists' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileDoesNotExist "${MSG}" $TH_EXISTING_DIRECTORY >"${stdoutF}" 2>"${stderrF}" )
+  th_assertTrueWithNoOutput 'directory exists, with msg' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileDoesNotExist >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithError 'too few arguments' $? "${stdoutF}" "${stderrF}"
+
+  ( assertFileDoesNotExist arg1 arg2 arg3 >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithError 'too many arguments' $? "${stdoutF}" "${stderrF}"
+}
+
+testAssertDirectoryExists() {
+  ( assertDirectoryExists $TH_EXISTING_DIRECTORY >"${stdoutF}" 2>"${stderrF}" )
+  th_assertTrueWithNoOutput 'directory exists' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryExists "${MSG}" $TH_EXISTING_DIRECTORY >"${stdoutF}" 2>"${stderrF}" )
+  th_assertTrueWithNoOutput 'directory exists, with msg' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryExists $TH_NON_EXISTING_DIRECTORY >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithOutput 'directory does not exist' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryExists "${MSG}" $TH_NON_EXISTING_DIRECTORY >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithOutput 'directory does not exist, with msg' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryExists $TH_EXISTING_FILE >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithOutput 'file exists' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryExists "${MSG}" $TH_EXISTING_FILE >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithOutput 'file exists, with msg' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryExists >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithError 'too few arguments' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryExists arg1 arg2 arg3 >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithError 'too many arguments' $? "${stdoutF}" "${stderrF}"
+}
+
+testAssertDirectoryDoesNotExist() {
+  ( assertDirectoryDoesNotExist $TH_NON_EXISTING_DIRECTORY >"${stdoutF}" 2>"${stderrF}" )
+  th_assertTrueWithNoOutput 'directory does not exist' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryDoesNotExist "${MSG}" $TH_NON_EXISTING_DIRECTORY >"${stdoutF}" 2>"${stderrF}" )
+  th_assertTrueWithNoOutput 'directory does not exist, with msg' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryDoesNotExist $TH_EXISTING_DIRECTORY >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithOutput 'directory exists' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryDoesNotExist "${MSG}" $TH_EXISTING_DIRECTORY >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithOutput 'directory exists, with msg' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryDoesNotExist $TH_EXISTING_FILE >"${stdoutF}" 2>"${stderrF}" )
+  th_assertTrueWithNoOutput 'file exists' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryDoesNotExist "${MSG}" $TH_EXISTING_FILE >"${stdoutF}" 2>"${stderrF}" )
+  th_assertTrueWithNoOutput 'file exists, with msg' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryDoesNotExist >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithError 'too few arguments' $? "${stdoutF}" "${stderrF}"
+
+  ( assertDirectoryDoesNotExist arg1 arg2 arg3 >"${stdoutF}" 2>"${stderrF}" )
+  th_assertFalseWithError 'too many arguments' $? "${stdoutF}" "${stderrF}"
+}
+
 oneTimeSetUp() {
   th_oneTimeSetUp
 }
